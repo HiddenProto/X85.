25061c74a4a5a1e02c81d4d699f551743d5ec950
HiddenProto <66harpseal66@gmail.com>
2025-11-26 16:23:29 -0500

    Update uv.config.js

--- uv.config.js
+++ uv.config.js
@@ -1,11 +1,6 @@
 self.__uv$config = {
-  prefix: '/service/',
+  prefix: '/~/',
   bare: 'https://uv.tls.b64.captain/',
   encodeUrl: Ultraviolet.codec.xor.encode,
-  decodeUrl: Ultraviolet.codec.xor.decode,
-  handler: '/uv.handler.js',
-  bundle: '/uv.bundle.js',
-  config: '/uv.config.js',
-  sw: '/uv.sw.js',
-  client: '/uv.client.js'
+  decodeUrl: Ultraviolet.codec.xor.decode
 };
