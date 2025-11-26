0ed4da7f8fda96c566782a075d9259b43a4730f5
HiddenProto <66harpseal66@gmail.com>
2025-11-26 16:04:53 -0500

    Add UV config

---
+++ uv.config.js
@@ -0,0 +1,11 @@
+window.__uv$config = {
+    prefix: '/uv/',
+    bare: 'https://projectultra.loca.lt/bare/',
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    bundle: '/uv/uv.bundle.js',
+    config: '/uv/uv.config.js',
+    sw: '/uv/uv.sw.js',
+    client: '/uv/uv.client.js',
+    handler: '/uv/uv.handler.js'
+};
